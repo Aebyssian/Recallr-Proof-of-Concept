cb5f2ed15ed6925e4030d79de891d02df84adbc8
Jacob Wilson <jbw622@msstate.edu>
2023-03-09 10:07:15 -0600

    Initial Commit

---
+++ Recallr Testing 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recallr Testing 2
+//
+//  Created by Jacob Wilson on 3/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Recallr Testing 2UITests/Recallr_Testing_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Recallr_Testing_2UITestsLaunchTests.swift
+//  Recallr Testing 2UITests
+//
+//  Created by Jacob Wilson on 3/9/23.
+//
+
+import XCTest
+
+final class Recallr_Testing_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

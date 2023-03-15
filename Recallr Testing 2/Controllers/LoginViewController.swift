871605f9dea000c189812497ac970fa4f4ba5284
Jacob Wilson <jbw622@msstate.edu>
2023-03-15 12:07:05 -0500

    Login

--- Recallr Testing 2/Controllers/LoginViewController.swift
+++ Recallr Testing 2/Controllers/LoginViewController.swift
@@ -6,9 +6,35 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var loginInButton: UIButton!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        errorLabel.isHidden = true
     }
+    
+    
+    @IBAction func loginInButtonPressed(_ sender: UIButton) {
+        if let email = emailField.text, let password = passwordField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    self.errorLabel.text = e.localizedDescription
+                    self.errorLabel.isHidden = false
+                } else {
+                    //self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+                // ...
+            }
+        }
+    }
+    
+    
+    
 }

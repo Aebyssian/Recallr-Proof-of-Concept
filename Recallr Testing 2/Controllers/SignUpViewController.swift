940c1ba57c21dbe07c1e83a09add898eda14961e
Jacob Wilson <jbw622@msstate.edu>
2023-03-15 11:45:23 -0500

    Fixed and finished registration

--- Recallr Testing 2/Controllers/SignUpViewController.swift
+++ Recallr Testing 2/Controllers/SignUpViewController.swift
@@ -12,8 +12,7 @@ import Firebase
 //should replace with some while loops
 //initially the passwordConfirmField kept returning nil so that's why this code is kinda weird
 
-class SignUpViewController: UIViewController, UITextFieldDelegate {
-    
+class SignUpViewController: UIViewController {
     
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
@@ -21,48 +20,41 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var passwordConfirmField: UITextField!
     @IBOutlet weak var registerButton: UIButton!
     
-    
     override func viewDidLoad() {
         doNotMatchLabel.isHidden = true
         super.viewDidLoad()
-        passwordConfirmField.delegate = self
-        passwordField.delegate = self
-        
         print("View loaded")
-        
-        
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        if let pass = passwordField.text {
-            if pass != passwordConfirmField.text ?? "" { //it can't unwrap it!
-                doNotMatchLabel.isHidden = false
-            }
-            else {
-                doNotMatchLabel.isHidden = true
-                doNotMatchLabel.text = "Passwords do not match"
-            }
-        }
     }
     
     @IBAction func registerButtonPressed(_ sender: UIButton) {
         
         if let pass = passwordField.text {
             if pass == passwordConfirmField.text ?? "" {
-                if let email = emailField.text, let password = passwordField.text {
-                    Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let email = emailField.text {
+                    Auth.auth().createUser(withEmail: email, password: pass) { authResult, error in
                         
                         if let e = error {
-                            print(e) //set doNotMatchLabel to localized description of error
+                            let desc = e.localizedDescription
+                            self.doNotMatchLabel.text = desc
+                            self.doNotMatchLabel.isHidden = false
                         } else {
                             //self.performSegue(withIdentifier: "RegisterToChat", sender: self)
                         }
                     }
                 }
+                else {
+                    doNotMatchLabel.text = "Please enter an email."
+                    doNotMatchLabel.isHidden = false
+                }
+            }
+            else {
+                doNotMatchLabel.text = "Passwords do not match."
+                doNotMatchLabel.isHidden = false
             }
         }
+        else {
+            doNotMatchLabel.text = "Please enter a password."
+            doNotMatchLabel.isHidden = false
+        }
     }
-    
-    
-    
 }
